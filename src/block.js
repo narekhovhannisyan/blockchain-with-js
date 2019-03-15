6c645574677222e2497b340df3a8397eb167e3dd
narekhovhannisyan <narekgilmour@gmail.com>
2019-03-15 16:11:30 +0400

    src: add `previousHash` argument to Block constructor.
     - add `checkPreviousHash` method to Block class.

--- src/block.js
+++ src/block.js
@@ -4,11 +4,11 @@ const crypto = require('crypto')
  * @class Block - Single blockchain node.
  */
 class Block {
-  constructor (index, data) {
+  constructor (index, data, previousHash) {
     this.index = index
     this.timestamp = Date.now()
     this.data = data
-    this.previousHash = null
+    this.previousHash = this.checkPreviousHash(previousHash)
     this.hash = this.buildHash()
   }
 
@@ -20,4 +20,21 @@ class Block {
     const dataToEncrypt = this.index + this.timestamp + this.previousHash + JSON.stringify(this.data)
     return crypto.createHash('sha256').update(dataToEncrypt).digest('hex')
   }
+
+  /**
+   * In case if there is no `previousHash`, throws error.
+   *  Otherwise returns given `previousHash`.
+   * @param {string|null} previousHash
+   * @return {string|null}
+   * @throws Error
+   */
+  checkPreviousHash (previousHash) {
+    if (this.index !== 0 && !previousHash) {
+      throw new Error('Previous hash is missing.')
+    }
+
+    return previousHash
+  }
 }
+
+module.exports = Block
